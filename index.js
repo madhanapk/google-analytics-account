76d905f224ebb8f63cec5db4e87286df511372d4
madhan <madhankumar.vsp@gmail.com>
2020-05-23 19:06:38 +0530

    authentication changed to service to service type

--- index.js
+++ index.js
@@ -16,109 +16,50 @@
 'use strict';
 
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const app = express();
-
 const { google } = require('googleapis');
 const port = process.env.PORT || 8080;
-
-const keyPath = path.join(__dirname, 'oauth2.keys.json');
 const analytics = google.analytics('v3');
-let keys = { redirect_uris: [''] };
-let token = "";
-if (fs.existsSync(keyPath)) {
-    keys = require(keyPath).installed;
-}
-const redirectUrl = (process.env.NODE_ENV === 'production') ? keys.redirect_uris[0] : keys.redirect_uris[1]
-
 
 /**
  * Create a new OAuth2 client with the configured keys.
  */
-const oauth2Client = new google.auth.OAuth2(
-    keys.client_id,
-    keys.client_secret,
-    redirectUrl
+// const oauth2Client = new google.auth.OAuth2(
+//     keys.client_id,
+//     keys.client_secret,
+//     redirectUrl
     
-);
+// );
 app.set('view engine', 'pug')
 
 /**
  * This is one of the many ways you can configure googleapis to use authentication credentials.  In this method, we're setting a global reference for all APIs.  Any other API you use here, like google.drive('v3'), will now use this auth client. You can also override the auth client at the service and method call levels.
  */
-google.options({ auth: oauth2Client });
 
 
-// oauth2Client.refreshAccessToken(function(err, tokens){
-//     console.log(tokens)
-//     oauth2Client.credentials = {access_token : tokens.access_token}
-// });
-
-oauth2Client.on('tokens', (tokens) => {
-    if (tokens.refresh_token) {
-        // store the refresh_token in my database!
-        console.log(tokens.refresh_token);
-    }
-    console.log(tokens.access_token);
+const googleAuth = new google.auth.GoogleAuth({
+    // Scopes can be specified either as an array or as a single, space-delimited string.
+    keyFile: './service-key.json',
+    scopes: ['https://www.googleapis.com/auth/analytics']
 });
-
-
+google.options({auth:googleAuth})
 
 /**
  * Open an http server to accept the oauth callback. In this simple example, the only request to our webserver is to /callback?code=<code>
  */
-async function authenticate(scopes) {
-    return new Promise((resolve, reject) => {
-        // grab the url that will be used for authorization
-        const authorizeUrl = oauth2Client.generateAuthUrl({
-            access_type: 'offline',
-            scope: scopes.join(' '),
-        });
+async function auth() {
+    return new Promise(async(resolve, reject) => {
         app.use(express.static('public'));
-        app.get('/', function (req, res) {
-            console.log(token);
-            if (token) {
-                res.redirect('/data')
-            } else {
-                res.render(
-                    'index', { href: authorizeUrl, label: 'Sign in' }
-                );
-            }
-        })
-
-        app.get('/data', async function (req, res) {
+        app.get('/', async function (req, res) {
             let list = {};
             list = await analytics.management.accounts.list()
             res.render(
-                'list', { list: JSON.stringify(list, null, 4) }
+                'list', { list: JSON.stringify(list.data, null, 4) }
             );
 
     })
-
-    app.use('/callbackredirect', function (req, res, next) {
-        token = req.query.code
-        oauth2Client.getToken(token)
-            .then((res) => {
-                console.log("tokenRes", res)
-                oauth2Client.setCredentials(res.tokens);
-                next()
-            })
-            .catch((err) => next())
-    });
-    app.get('/callbackredirect', function (req, res) {
-
-        if (token) {
-            res.redirect('/data')
-        } else {
-            res.redirect('/')
-        }
-        // res.send(
-        //     analytics.management.accounts.list()
-        // );
-    });//
     app.listen(port, () => {
-        resolve(oauth2Client);
+        resolve();
     });
 });
 }
@@ -128,11 +69,11 @@ async function runSample(client) {
     //const res = await plus.people.get({userId: 'me'});
     //const res = await plus.people.get({userId: 'me'});
     //console.log(analytics);
-    console.log("test");
+    console.log(client);
 }
 
 const scopes = ['https://www.googleapis.com/auth/analytics.readonly'];
-authenticate(scopes)
+auth(scopes)
     .then(client => runSample(client))
     .catch(console.error);
 
